c4fbb8e87896469300aef6b6975bca94681f5ea2
Gustavo Silva <gustavo.silva192@gmail.com>
2024-10-31 07:40:45 -0300

    Changes DB to postgresql

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for
 from models import db, Product, Price
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:eWbcsVjtfEMfledXaccIgQkeCpDlbwGJ@postgres.railway.internal:5432/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
