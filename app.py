822b932b0b85b218fe32bcbced460ac51761ebc4
Gustavo Silva <gustavo.silva192@gmail.com>
2024-10-31 07:24:28 -0300

    Change start in app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ if __name__ == '__main__':
     # with app.app_context():
     #      db.drop_all()  # Cuidado: isso remove todas as tabelas e dados
     #      db.create_all()  # Cria as tabelas novamente
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
